e74a77ceefef3bf708324e9d66ad3d5540deb760
Alberto Dubois <aduboisr@gmail.com>
2014-05-07 10:03:11 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -10,19 +10,20 @@ hpower$Date2 = strptime(hpower$Date2,"%Y-%m-%d %H:%M:%S")
 
 #Make the plot
 png("plot4.png", width = 480, height = 480)
-par(mfcol=c(2,2)) #Grid of plots
+par(mfrow=c(2,2)) #Grid of plots
 
 #First plot
 plot(hpower$Date2,hpower$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 
 #Second plot
+plot(hpower$Date2, hpower$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+#Third plot
 plot(hpower$Date2, hpower$Sub_metering_1, type="l",ylab="Energy Submetering", xlab="")
 lines(hpower$Date2, hpower$Sub_metering_2, type="l", col="red")
 lines(hpower$Date2, hpower$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd =2.5,col=c("black", "red", "blue"),bty="n")
 
-#Third plot
-plot(hpower$Date2, hpower$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 #Fourth plot
 plot(hpower$Date2, hpower$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")

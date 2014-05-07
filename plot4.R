7477151debbc9f769b244406b7b7d3deb42c1e1f
Alberto Dubois <aduboisr@gmail.com>
2014-05-07 02:19:44 +0200

    Delete

--- plot4.R
+++ plot4.R
@@ -4,11 +4,12 @@ link = "household_power_consumption.txt"
 hpower = read.table("household_power_consumption.txt",skip = 66637, nrow = 2880, sep = ";",col.names = colnames(read.table("household_power_consumption.txt",nrow = 1, header = TRUE,sep=";")))
 
 hpower$Date=as.Date(hpower$Date,format="%d/%m/%Y")
-hpower$Date2=paste(sub2$Date,sub2$Time)
+hpower$Date2=paste(hpower$Date,hpower$Time)
 hpower$Date2 = strptime(hpower$Date2,"%Y-%m-%d %H:%M:%S")
 
 
 #Make the plot
+png("plot4.png", width = 480, height = 480)
 par(mfcol=c(2,2)) #Grid of plots
 
 #First plot
@@ -18,7 +19,7 @@ plot(hpower$Date2,hpower$Global_active_power,type="l",ylab="Global Active Power 
 plot(hpower$Date2, hpower$Sub_metering_1, type="l",ylab="Energy Submetering", xlab="")
 lines(hpower$Date2, hpower$Sub_metering_2, type="l", col="red")
 lines(hpower$Date2, hpower$Sub_metering_3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd =2.5,col=c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd =2.5,col=c("black", "red", "blue"),bty="n")
 
 #Third plot
 plot(hpower$Date2, hpower$Voltage, type="l", xlab="datetime", ylab="Voltage")
@@ -27,5 +28,4 @@ plot(hpower$Date2, hpower$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(hpower$Date2, hpower$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 
-dev.copy(png,file="plot4.png",width=480,height=480)
 dev.off()
